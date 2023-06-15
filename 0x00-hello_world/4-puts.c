130ec154b54bfec7a4b61f6291e95c0e7bc97113
bruzwyn <bruce.koech.7@gmail.com>
2023-06-15 07:45:38 -0700

    preprocessor

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * description main prints out string to stdout.
+ * returns 0 if it succeeds.
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
