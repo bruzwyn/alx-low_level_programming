cbb73ee0e48529fb2ad710004ab592f590cfe172
bruzwyn <bruce.koech.7@gmail.com>
2023-06-16 14:48:46 -0700

    c program

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 on success
+ */
+int main(void)	
+{
+int n = 97;
+int y = 65;
+while (n <= 122)
+{
+putchar(n);
+}
+while (y <= 90)
+{
+putchar(y);
+}
+putchar('\n');
+return (0);
+}
