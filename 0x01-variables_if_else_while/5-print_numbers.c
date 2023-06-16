729b835442b56573086abe800b5728bba0f6ff4f
bruzwyn <bruce.koech.7@gmail.com>
2023-06-16 15:42:53 -0700

    c program

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
