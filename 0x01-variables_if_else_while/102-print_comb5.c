d104e3ec4b7ecf02e97430ab2c44cbe2af099597
bruzwyn <bruce.koech.7@gmail.com>
2023-06-16 19:20:35 -0700

    c program

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 int q, p;
-for (p = '0'; p < 98; p++)
+for (p = '0'; p <= 99; p++)
 {
 for (q = p + 1; q <= 99; q++)
 {
@@ -18,7 +18,7 @@ putchar((p % 10) + '0');
 putchar(' ');
 putchar((q / 10) + '0');
 putchar((q % 10) + '0');
-if (p == 98 && q == 99)
+if (p == 99 && q == 99)
 continue;
 putchar(',');
 putchar(' ');
