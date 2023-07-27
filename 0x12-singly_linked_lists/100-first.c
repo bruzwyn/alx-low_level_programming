8d8f8310d4b4351fd7a96696c663b107d7edb4b0
bruzwyn <bruce.koech.7@gmail.com>
2023-07-27 15:15:32 -0700

    linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void first(void) __attribute__ ((constructor));
  */
 void first(void)
 {
-	printf("You're gorgeous and you know it,\n");
-	printf("i love my dog!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
