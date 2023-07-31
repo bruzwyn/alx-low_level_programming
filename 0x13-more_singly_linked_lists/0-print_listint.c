d06322334d6cf7a2f99aa9fb57a20f6abb2b515a
bruzwyn <bruce.koech.7@gmail.com>
2023-07-31 14:25:58 -0700

    linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,14 +2,14 @@
 #include "lists.h"
 
 /**
- * listint_len - prints all the elements of a list.
+ * print_listint - prints all the elements of a list.
  * @h: variable list to print
  * Description: singly linked list node structure
  *
  * Return: number of elements
  */
 
-size_t listint_len(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 size_t count = 0;
 
