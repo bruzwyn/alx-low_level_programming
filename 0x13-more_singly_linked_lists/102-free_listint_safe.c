ef713c7e00731890c242f9d5c0563960501f341b
root <bruce.koech.7@gmail.com>
2023-11-21 15:01:10 -0800

    my commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,19 +8,19 @@
  */
 void free_listp2(listp_t **head)
 {
-    listp_t *temp;
-    listp_t *curr;
+listp_t *temp;
+listp_t *curr;
 
-    if (head != NULL)
-    {
-        curr = *head;
-        while ((temp = curr) != NULL)
-        {
-            curr = curr->next;
-            free(temp);
-        }
-        *head = NULL;
-    }
+if (head != NULL)
+{
+curr = *head;
+while ((temp = curr) != NULL)
+{
+curr = curr->next;
+free(temp);
+}
+*head = NULL;
+}
 }
 
 /**
@@ -31,41 +31,41 @@ void free_listp2(listp_t **head)
  */
 size_t free_listint_safe(listint_t **head)
 {
-    size_t nnodes = 0;
-    listp_t *hptr = NULL, *new, *add;
-    listint_t *curr;
+size_t nnodes = 0;
+listp_t *hptr = NULL, *new, *add;
+listint_t *curr;
 
-    while (*head != NULL)
-    {
-        new = malloc(sizeof(listp_t));
+while (*head != NULL)
+{
+new = malloc(sizeof(listp_t));
 
-        if (new == NULL)
-            exit(98);
+if (new == NULL)
+exit(98);
 
-        new->p = (void *)*head;
-        new->next = hptr;
-        hptr = new;
+new->p = (void *)*head;
+new->next = hptr;
+hptr = new;
 
-        add = hptr;
+add = hptr;
 
-        while (add->next != NULL)
-        {
-            add = add->next;
-            if (*head == add->p)
-            {
-                *head = NULL;
-                free_listp2(&hptr);
-                return (nnodes);
-            }
-        }
+while (add->next != NULL)
+{
+add = add->next;
+if (*head == add->p)
+{
+*head = NULL;
+free_listp2(&hptr);
+return (nnodes);
+}
+}
 
-        curr = *head;
-        *head = (*head)->next;
-        free(curr);
-        nnodes++;
-    }
+curr = *head;
+*head = (*head)->next;
+free(curr);
+nnodes++;
+}
 
-    *head = NULL;
-    free_listp2(&hptr);
-    return (nnodes);
+*head = NULL;
+free_listp2(&hptr);
+return (nnodes);
 }
